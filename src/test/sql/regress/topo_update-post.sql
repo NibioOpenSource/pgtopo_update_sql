8aa69dbba233112c5a4f215624f7fefcf1fe3da7
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-09-04 07:49:15 +0200

    Removed sql for nibio_reg

--- src/test/sql/regress/topo_update-post.sql
+++ src/test/sql/regress/topo_update-post.sql
@@ -3,9 +3,4 @@ select topology.droptopology('topo_rein_sysdata');
 drop schema topo_update cascade;
 
 DROP OWNED BY topo_rein cascade;
-DROP OWNED BY topo_update_crud1 cascade;
 DROP OWNED BY topo_update cascade;
-
-DROP role topo_rein;
-DROP role topo_update_crud1;
-DROP role topo_update;
