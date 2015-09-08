5960b4ec8f34cdac5c54e7a515267796b137aa2f
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-09-08 11:18:36 +0200

    Clean up data in a correct way database already exit

--- src/test/sql/help_scripts/reset_all_rein_data.sh
+++ src/test/sql/help_scripts/reset_all_rein_data.sh
@@ -1,13 +1,13 @@
 echo 'drop schema topo_rein cascade;' > /tmp/trein.sql
+echo 'drop schema topo_update cascade;'  >> /tmp/trein.sql
 echo "select topology.droptopology('topo_rein_sysdata');"  >> /tmp/trein.sql
-echo 'drop schema topo_update cascade1;'  >> /tmp/trein.sql
 cat ~/dev/git/geomatikk/dbsql/src/db/sl/topo_rein/schema*.sql  >> /tmp/trein.sql 
 cat ~/dev/git/geomatikk/dbsql/src/db/sl/topo_rein/roles_topo_rein.sql  >> /tmp/trein.sql 
 cat ~/dev/git/geomatikk/dbsql/src/db/sl/topo_rein/tables_01_kode_topo_rein.sql >> /tmp/trein.sql
 cat ~/dev/git/geomatikk/dbsql/src/db/sl/topo_rein/table_02_arstidsbeite_var.sql >> /tmp/trein.sql
 cat ~/dev/git/geomatikk/dbsql/src/db/sl/topo_rein/view_01_arstidsbeite_var.sql >> /tmp/trein.sql
 cat ~/dev/git/topologi/pgtopo_update/kode/trunk/src/main/sql/topo_update/schema_topo_update.sql  >> /tmp/trein.sql 
-cat ~/dev/git/topologi/pgtopo_update_sql/src/main/sql/topo_update/schema_userdef*  >> /tmp/trein.sql
+cat ~/dev/git/topologi/pgtopo_update_sql/src/main/sql/topo_update/schema_*  >> /tmp/trein.sql
 cat ~/dev/git/topologi/pgtopo_update_sql/src/main/sql/topo_update/roles_topo_update.sql >> /tmp/trein.sql
 cat ~/dev/git/topologi/pgtopo_update_sql/src/main/sql/topo_update/function*  >> /tmp/trein.sql
 cat /tmp/trein.sql
