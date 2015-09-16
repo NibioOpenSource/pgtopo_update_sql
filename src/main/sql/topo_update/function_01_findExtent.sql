7b4c8db23574483961f4b65a71eff16d027aca61
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-09-16 13:14:00 +0200

    return geomtry type and null if no extent is found

--- src/main/sql/topo_update/function_01_findExtent.sql
+++ src/main/sql/topo_update/function_01_findExtent.sql
@@ -1,11 +1,10 @@
 
 CREATE OR REPLACE FUNCTION topo_rein.findExtent(schema_name text, table_name text , geocolumn_name text )
-RETURNS text AS $$
+RETURNS geometry AS $$
 DECLARE
-bb geometry;
+bb geometry = null;
 command_string text;
 BEGIN
-	--	TODO fix function
 	IF (EXISTS 
 			( SELECT * FROM topo_rein_sysdata.edge_data  limit 1)
 	) THEN
@@ -13,17 +12,10 @@ BEGIN
 			SELECT ST_EstimatedExtent(schema_name,table_name, geocolumn_name)::geometry into bb;
         EXCEPTION WHEN internal_error THEN
         -- ERROR:  XX000: stats for "edge_data.geom" do not exist
-        	-- Catch error and return a empty polygon ant he let application decide what to do
-   			SELECT ST_GeomFromText('POLYGON EMPTY') into bb;
-
+        -- Catch error and return a return null ant let application decide what to do
         END;
-	ELSE
-		SELECT ST_GeomFromText('POLYGON EMPTY') into bb;
 	END IF;
 	
-    RAISE NOTICE '%', St_AsText(bb);
-	
-    -- This is hack needed by the cleint
-	RETURN St_AsText(bb);
+	RETURN bb;
 END;
 $$ LANGUAGE 'plpgsql' VOLATILE;
