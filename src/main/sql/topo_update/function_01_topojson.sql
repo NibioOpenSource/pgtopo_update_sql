e641c53906d902db6caa9279d90dbc72597c3486
Sandro Santilli <strk@keybit.net>
2015-09-28 19:48:30 +0200

    Add query_to_topojson function (#6)

--- src/main/sql/topo_update/function_01_topojson.sql
+++ src/main/sql/topo_update/function_01_topojson.sql
@@ -1,3 +1,123 @@
+-- Return a topojson document with the contents of the query results
+-- topojson specs:
+-- https://github.com/mbostock/topojson-specification/blob/master/README.md
+--
+-- NOTE: will use TopoGeometry identifier as the feature identifier
+--
+--{
+CREATE OR REPLACE FUNCTION topo_rein.query_to_topojson(query text, srid_out int, maxdecimaldigits int)
+RETURNS text AS
+$$
+DECLARE
+  tmptext text;
+  outary text[];
+  objary text[];
+  rec RECORD;
+  rec2 RECORD;
+  json_result text;
+  fname_topogeom text;
+  fname_attributes text[];
+  typname text;
+  toponame text;
+  topology_id int;
+  sql text;
+BEGIN
+
+
+  outary := ARRAY['{"type":"Topology","objects":{'];
+
+  CREATE TEMP TABLE topo_rein_topojson_edgemap
+      (arc_id serial primary key, edge_id int);
+
+  -- Find TopoGeometry and attributes names
+  sql := 'SELECT * FROM ( ' || query || ') foo LIMIT 1';
+  FOR rec IN EXECUTE sql
+  LOOP
+    FOR rec2 IN SELECT json_object_keys(row_to_json(rec)) fn
+    LOOP
+      sql := 'SELECT pg_typeof(' || quote_ident(rec2.fn) ||
+              ') FROM ( ' || query ||
+              ') foo LIMIT 1';
+      --RAISE DEBUG '%', sql;
+      EXECUTE sql INTO STRICT typname;
+      RAISE NOTICE 'Field: % of type %', rec2.fn, typname;
+      IF typname = 'topogeometry' THEN
+        IF fname_topogeom IS NULL THEN
+          fname_topogeom := rec2.fn;
+        ELSE
+          RAISE WARNING 'TopoGeometry field "%" ignored as we found "%" already', rec2.fn, fname_topogeom;
+        END IF;
+      ELSIF typname = 'geometry' OR typname = 'geography' THEN
+          RAISE WARNING '"%" field "%" ignored', typname, rec2.fn;
+      ELSE
+        fname_attributes := fname_attributes || quote_ident(rec2.fn);
+      END IF;
+    END LOOP;
+  END LOOP;
+
+  IF fname_topogeom IS NULL THEN
+    RAISE EXCEPTION 'No TopoGeometry field in query result';
+  END IF;
+
+  -- Add features (objects) array
+
+  sql := 'SELECT id(' || quote_ident(fname_topogeom) ||
+      '), AsTopoJSON(' || quote_ident(fname_topogeom) ||
+      ', ''topo_rein_topojson_edgemap'') as obj, ' || 
+      'topology_id(' || quote_ident(fname_topogeom) || 
+      '), row_to_json( (SELECT t1 FROM ( SELECT ' ||
+      -- attributes to include in output
+      array_to_string(fname_attributes, ',') ||
+      ') as t1)) as prop ' || 
+      ' FROM ( ' || query || ' ) foo';
+  FOR rec IN EXECUTE sql
+  LOOP
+    IF topology_id IS NULL THEN
+      topology_id := rec.topology_id;
+    ELSIF topology_id != rec.topology_id THEN
+      RAISE EXCEPTION 'TopoGeometry from different topologies mixed in query results';
+    END IF;
+    objary := objary || array_to_string(ARRAY[
+          '"', rec.id::text, '":',
+          substring(rec.obj from 0 for length(rec.obj)),
+          ',"properties":',
+          rec.prop::text,
+          '}'
+          ], '');
+  END LOOP;
+
+  sql := 'SELECT name FROM topology.topology WHERE id = ' || topology_id;
+  EXECUTE sql INTO STRICT toponame;
+
+  outary := outary || array_to_string(objary, ',');
+
+  outary := outary || '},"arcs": ['::text;
+
+  -- Add arcs
+
+  sql := 'SELECT array_agg(ST_AsGeoJSON(' ||
+      'ST_transform(e.geom,$1),$2' ||
+      ')::json->>''coordinates'' ' ||
+      'ORDER BY m.arc_id) FROM topo_rein_topojson_edgemap m ' ||
+      'INNER JOIN ' || quote_ident(toponame) || '.edge e ' ||
+      'ON (e.edge_id = m.edge_id)';
+  RAISE DEBUG '%', sql;
+  EXECUTE sql USING srid_out,maxdecimaldigits INTO objary;
+
+  outary = outary || array_to_string(objary, ',');
+
+  outary = outary || ']}'::text;
+
+  RAISE DEBUG '%', array_to_string(outary, '');
+  
+  DROP TABLE topo_rein_topojson_edgemap;
+  
+  json_result = array_to_string(outary, '')::varchar;
+  RETURN json_result;
+
+END;
+$$ LANGUAGE 'plpgsql' VOLATILE;
+--}
 
 -- Return a topojson document with the contents of
 -- topo_rein.arstidsbeite_var_flate table
@@ -7,6 +127,9 @@
 -- env used to select bounding box area
 -- srid_out is the srid data put are transformed to
 -- maxdecimaldigits the number og digets used for update
+--
+-- TODO: re-write as a wrapper to query_to_topojson ?
+--{
 CREATE OR REPLACE FUNCTION topo_rein.get_var_flate_topojson(env box2d, srid_out int, maxdecimaldigits int)
 RETURNS text AS
 $$
@@ -73,6 +196,7 @@ BEGIN
 
 END;
 $$ LANGUAGE 'plpgsql' VOLATILE;
+--}
 
 CREATE OR REPLACE FUNCTION topo_rein.get_var_flate_topojson(srid_out int, maxdecimaldigits int)
 RETURNS TEXT
@@ -85,9 +209,13 @@ $$ LANGUAGE 'sql' VOLATILE;
 
 -- This function sould be replaced by the function avouve
 
+--{
 -- env used to select bounding box area
 -- srid_out is the srid data put are transformed to
 -- maxdecimaldigits the number og digets used for update
+--
+-- TODO: deprecate in favour of query_to_topojson ?
+--
 CREATE OR REPLACE FUNCTION topo_rein.get_var_flate_topojson(_new_topo_objects regclass, srid_out int, maxdecimaldigits int)
 RETURNS text AS
 $$
@@ -164,3 +292,4 @@ EXECUTE format('CREATE TEMP TABLE new_surface_data AS (SELECT * FROM %s)', _new_
 
 END;
 $$ LANGUAGE 'plpgsql' VOLATILE;
+--}
