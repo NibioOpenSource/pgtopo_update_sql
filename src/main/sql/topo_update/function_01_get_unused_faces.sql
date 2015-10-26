a69b00aef41520bd4bbbbf6b7994439c2cd767ea
Sandro Santilli <strk@keybit.net>
2015-10-26 12:24:47 +0100

    Add get_unused_faces function (#10)

---
+++ src/main/sql/topo_update/function_01_get_unused_faces.sql
@@ -0,0 +1,20 @@
+-- Return the faces that are not used by any TopoGeometry
+CREATE OR REPLACE FUNCTION topo_rein.get_unused_faces()
+RETURNS setof int AS
+$$
+    SELECT f.face_id as faces
+    FROM
+      topo_rein_sysdata.face f
+    WHERE f.face_id > 0
+      EXCEPT
+    SELECT r.element_id
+    FROM
+      topo_rein_sysdata.relation r,
+      topology.layer l
+    WHERE r.layer_id = l.layer_id 
+      and l.level = 0 -- non hierarchical
+      and r.element_type = 3 -- a face
+$$ LANGUAGE 'sql' VOLATILE;
+
+-- Return a set of identifiers for edges that are not covered
+-- by any surface TopoGeometry
--- src/test/sql/regress/topo_update-post.sql
+++
@@ -1,7 +0,0 @@
---DROP OWNED BY topo_rein cascade;
---DROP OWNED BY topo_update cascade;
-
-drop schema topo_rein cascade;
-select topology.droptopology('topo_rein_sysdata');
-drop schema topo_update cascade;
-
