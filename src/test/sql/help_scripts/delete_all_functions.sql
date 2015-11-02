5d3deccd29fdd54a1f660c772ff5ad441567adc3
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-11-02 08:59:00 +0100

    Added new functions

--- src/test/sql/help_scripts/delete_all_functions.sql
+++ src/test/sql/help_scripts/delete_all_functions.sql
@@ -5,6 +5,7 @@ DROP FUNCTION IF EXISTS topo_rein.get_rein_felles_egenskaper_flate(localid_in in
 DROP FUNCTION IF EXISTS topo_update.get_linestring_no_loose_ends(topo_info topo_update.input_meta_info, topo topogeometry) cascade; 
 DROP FUNCTION IF EXISTS topo_update.get_topo_layer_id(topo_info topo_update.input_meta_info) cascade; 
 DROP FUNCTION IF EXISTS topo_update.has_linestring_loose_ends(topo_info topo_update.input_meta_info, topo topogeometry) cascade; 
+DROP FUNCTION IF EXISTS topo_update.touches(_new_topo_objects regclass,id_to_check int)  cascade; 
 
 DROP FUNCTION IF EXISTS topo_rein.query_to_topojson(query text, srid_out int, maxdecimaldigits int) cascade;
 DROP FUNCTION IF EXISTS topo_rein.get_var_flate_topojson(env box2d, srid_out int, maxdecimaldigits int) cascade;
@@ -12,6 +13,7 @@ DROP FUNCTION IF EXISTS topo_rein.get_var_flate_topojson(srid_out int, maxdecima
 DROP FUNCTION IF EXISTS topo_rein.get_var_flate_topojson(_new_topo_objects regclass, srid_out int, maxdecimaldigits int) cascade;
 
 DROP FUNCTION IF EXISTS topo_update.create_edge_surfaces(new_border_data topogeometry) cascade; 
+DROP FUNCTION IF EXISTS topo_update.create_edge_surfaces(new_border_data topogeometry, valid_user_geometry geometry) cascade; 
 DROP FUNCTION IF EXISTS topo_update.create_surface_edge(geo_in geometry) cascade; 
 DROP FUNCTION IF EXISTS topo_update.update_domain_surface_layer(_new_topo_objects regclass) cascade; 
 
