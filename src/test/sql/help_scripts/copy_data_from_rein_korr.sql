0d551be3c1536afb0fa2e0d0e4be7230c70c6f02
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-11-05 06:34:09 +0100

    Add data from org tables also not only updated data

--- src/test/sql/help_scripts/copy_data_from_rein_korr.sql
+++ src/test/sql/help_scripts/copy_data_from_rein_korr.sql
@@ -227,10 +227,35 @@ FROM (
 delete from topo_rein.arstidsbeite_var_flate where reinbeitebruker_id is null;
 
 ------- Gjerder og Anlegg linje
--- from: org_rein_korr.rein_korr_drift_anlegg_linje 
+-- from:  org_rein.reindrift_anlegg_linje
 -- to: topo_rein.reindrift_anlegg_linje
 
 SELECT topo_update.create_line_edge_domain_obj(f.a) 
+FROM (
+ 	SELECT 
+ 	'{"type": "Feature",' || 
+ 	'"geometry":' || ST_AsGeoJSON(ST_setSrid(ST_transform(geo,4258),4258),10,2)::json || ',' ||
+ 	'"properties":' || row_to_json((SELECT l FROM (SELECT reinbeitebruker_id, reindriftsanleggstype, "felles_egenskaper.forstedatafangsdato", "felles_egenskaper.verifiseringsdato", "felles_egenskaper.oppdateringsdato","felles_egenskaper.opphav","felles_egenskaper.kvalitet.maalemetode") As l )) || '}' as a,
+ 	ST_setSrid(ST_transform(geo,4258),4258) as geo
+FROM 
+	( 
+		SELECT 
+		beitebrukerid AS reinbeitebruker_id, 
+		reindriftanltyp AS reindriftsanleggstype, 
+		forstedatafangstdato as "felles_egenskaper.forstedatafangsdato",
+		endretdato as "felles_egenskaper.verifiseringsdato",
+		oppdateringsdato as "felles_egenskaper.oppdateringsdato",
+		produsent as "felles_egenskaper.opphav",
+		82 as "felles_egenskaper.kvalitet.maalemetode",
+		geo 
+		FROM  org_rein.reindrift_anlegg_linje
+		where reindriftanltyp in (4,5,6,7)
+		ORDER BY objectid
+	) AS lg
+) As f;
+
+-- then read from korr, the layer thats is updated
+ SELECT topo_update.create_line_edge_domain_obj(f.a) 
 FROM (
  	SELECT 
  	'{"type": "Feature",' || 
@@ -255,9 +280,36 @@ FROM
 ) As f;
 
 
+
 ------- Gjerder og Anlegg punkt
--- from: org_rein_korr.rein_korr_drift_anlegg_punkt 
+-- from:  org_rein.reindrift_anlegg_punkt
 -- to: topo_rein.reindrift_anlegg_punkt
+
+SELECT topo_update. topo_update.create_point_point_domain_obj(f.a) 
+FROM (
+ 	SELECT 
+ 	'{"type": "Feature",' || 
+ 	'"geometry":' || ST_AsGeoJSON(ST_setSrid(ST_transform(geo,4258),4258),10,2)::json || ',' ||
+ 	'"properties":' || row_to_json((SELECT l FROM (SELECT reinbeitebruker_id, reindriftsanleggstype, "felles_egenskaper.forstedatafangsdato", "felles_egenskaper.verifiseringsdato", "felles_egenskaper.oppdateringsdato","felles_egenskaper.opphav","felles_egenskaper.kvalitet.maalemetode") As l )) || '}' as a,
+ 	ST_setSrid(ST_transform(geo,4258),4258) as geo
+FROM 
+	( 
+		SELECT 
+		beitebrukerid AS reinbeitebruker_id, 
+		reindriftanltyp AS reindriftsanleggstype, 
+		forstedatafangstdato as "felles_egenskaper.forstedatafangsdato",
+		endretdato as "felles_egenskaper.verifiseringsdato",
+		oppdateringsdato as "felles_egenskaper.oppdateringsdato",
+		produsent as "felles_egenskaper.opphav",
+		82 as "felles_egenskaper.kvalitet.maalemetode",
+		geo 
+		FROM  org_rein.reindrift_anlegg_punkt
+		WHERE reindriftanltyp in (10,11,12,13,14,15,16,17,18,19) and length(beitebrukerid) < 4
+		ORDER BY objectid
+	) AS lg
+) As f;
+
+-- then read from korr, the layer thats is updated
 
 SELECT topo_update.create_point_point_domain_obj(f.a) 
 FROM (
