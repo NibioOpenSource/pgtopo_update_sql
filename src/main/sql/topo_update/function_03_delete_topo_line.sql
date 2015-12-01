0b344780db1e0b272c6e6a1b106be672cff8c085
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-12-01 10:56:36 +0100

    Made generic delete for lines

--- src/main/sql/topo_update/function_03_delete_topo_line.sql
+++ src/main/sql/topo_update/function_03_delete_topo_line.sql
@@ -1,62 +1,74 @@
--- delete line that intersects with given point
+-- delete line with given id from given layer
 
-CREATE OR REPLACE FUNCTION topo_update.delete_topo_line(id_in int) 
+CREATE OR REPLACE FUNCTION topo_update.delete_topo_line(id_in int,layer_schema text, layer_table text, layer_column text) 
 RETURNS int AS $$DECLARE
-
 
 -- holds the num rows affected when needed
 num_rows_affected int;
--- TODO use as parameter put for testing we just have here for now
+
+-- hold common needed in this proc
 border_topo_info topo_update.input_meta_info ;
 
 -- holds dynamic sql to be able to use the same code for different
 command_string text;
 
-border_layer_id int;
-
 BEGIN
-	
-		-- TODO to be moved is justed for testing now
-	border_topo_info.topology_name := 'topo_rein_sysdata';
-	border_topo_info.layer_schema_name := 'topo_rein';
-	border_topo_info.layer_table_name := 'reindrift_anlegg_linje';
-	border_topo_info.layer_feature_column := 'linje';
-	border_topo_info.snap_tolerance := 0.0000000001;
-	border_topo_info.element_type = 2;
-		-- find border layer id
-		
-	
-	-- find border layer id
-	border_layer_id := topo_update.get_topo_layer_id(border_topo_info);
-	
 
-	    -- Find linear objects related to his edges 
+	-- get meta data
+	border_topo_info := topo_update.make_input_meta_info(layer_schema, layer_table , layer_column );
+
+	-- %s interpolates the corresponding argument as a string; %I escapes its argument as an SQL identifier; %L escapes its argument as an SQL literal; %% outputs a literal %.
+	
+	-- Find linear objects related to his edges 
+    command_string := FORMAT('
     DROP TABLE IF EXISTS ttt_edge_list;
     CREATE TEMP TABLE ttt_edge_list AS 
     (
 		select distinct ed.edge_id
 	    FROM 
-		topo_rein_sysdata.relation re,
-		topo_rein.reindrift_anlegg_linje al, 
-		topo_rein_sysdata.edge_data ed
+		%1$I.relation re,
+		%2$I.%3$I al, 
+		%1$I.edge_data ed
 		WHERE 
-		al.id = id_in AND
+		al.id = %4$L AND
 		(al.linje).id = re.topogeo_id AND
-		re.layer_id =  border_layer_id AND 
-		re.element_type = 2 AND  -- TODO use variable element_type_edge=2
+		re.layer_id =  %5$L AND 
+		re.element_type = %6$L AND  
 		ed.edge_id = re.element_id AND
-		NOT EXISTS ( SELECT 1 FROM topo_rein_sysdata.relation re2 WHERE  ed.edge_id = re2.element_id AND re2.topogeo_id != re.topogeo_id) 
+		NOT EXISTS ( SELECT 1 FROM %1$I.relation re2 WHERE  ed.edge_id = re2.element_id AND re2.topogeo_id != re.topogeo_id) 
+    )',
+    border_topo_info.topology_name,
+    border_topo_info.layer_schema_name,
+	border_topo_info.layer_table_name,
+	id_in,
+	border_topo_info.border_layer_id,
+	border_topo_info.element_type
+	);
+	RAISE NOTICE 'command_string %', command_string;
+  	EXECUTE command_string;
 
-    );
+   
+  	-- Clear the topogeom before delete
+	command_string := FORMAT('SELECT topology.clearTopoGeom(a.%I) FROM %I.%I  a WHERE a.id = %L',
+	border_topo_info.layer_feature_column,
+ 	border_topo_info.layer_schema_name,
+  	border_topo_info.layer_table_name,
+  	id_in
+  	);
+	RAISE NOTICE 'command_string %', command_string;
+  	EXECUTE command_string;
 
-    
-	PERFORM topology.clearTopoGeom(linje) FROM topo_rein.reindrift_anlegg_linje r
-	WHERE id = id_in;
+
+	-- Delete the line from the org table
+	command_string := FORMAT('DELETE FROM %I.%I a WHERE a.id = %L',
+	border_topo_info.layer_schema_name,
+  	border_topo_info.layer_table_name,
+  	id_in
+  	);
+	RAISE NOTICE 'command_string %', command_string;
+  	EXECUTE command_string;
 
 	
-	DELETE FROM topo_rein.reindrift_anlegg_linje r
-	WHERE id = id_in;
-
 	GET DIAGNOSTICS num_rows_affected = ROW_COUNT;
 
 	RAISE NOTICE 'Rows deleted  %',  num_rows_affected;
@@ -68,21 +80,29 @@ BEGIN
 			SELECT ST_RemEdgeModFace(%1$L, ed.edge_id)
 			FROM 
 			ttt_edge_list ued,
-			%2$s ed
+			%1$I.edge_data ed
 			WHERE 
 			ed.edge_id = ued.edge_id 
 			',
-			border_topo_info.topology_name,
-			border_topo_info.topology_name || '.edge_data'
+			border_topo_info.topology_name
 		);
 
 	RAISE NOTICE 'command_string %', command_string;
-
 	EXECUTE command_string;
-
 	
 	RETURN num_rows_affected;
 
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+--{ kept for backward compatility
+CREATE OR REPLACE FUNCTION topo_update.delete_topo_line(id_in int) 
+RETURNS TABLE(id integer) AS $$
+  SELECT topo_update.delete_topo_line($1, 'topo_rein', 'reindrift_anlegg_linje', 'linje');
+$$ LANGUAGE 'sql';
+--}
+
+
 
