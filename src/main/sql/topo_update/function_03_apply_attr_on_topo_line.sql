19f5554091f9302164fddbcbfcf20a709021065e
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-12-02 11:03:59 +0100

    Added generic handling

--- src/main/sql/topo_update/function_03_apply_attr_on_topo_line.sql
+++ src/main/sql/topo_update/function_03_apply_attr_on_topo_line.sql
@@ -1,21 +1,14 @@
 
-
 -- update attribute values for given topo object
-CREATE OR REPLACE FUNCTION topo_update.apply_attr_on_topo_line(json_feature text) 
+
+CREATE OR REPLACE FUNCTION topo_update.apply_attr_on_topo_line(json_feature text,
+  layer_schema text, layer_table text, layer_column text) 
 RETURNS int AS $$DECLARE
 
 num_rows int;
 
-
--- this line layer id will picked up by input parameters
-line_layer_id int;
-
-
--- TODO use as parameter put for testing we just have here for now
-line_topo_info topo_update.input_meta_info ;
-
--- hold striped gei
-edge_with_out_loose_ends geometry = null;
+-- common meta info
+topo_info topo_update.input_meta_info ;
 
 -- holds dynamic sql to be able to use the same code for different
 command_string text;
@@ -23,58 +16,105 @@ command_string text;
 -- holds the num rows affected when needed
 num_rows_affected int;
 
--- used to hold values
-felles_egenskaper_flate topo_rein.sosi_felles_egenskaper;
+-- used to hold values temp values
 simple_sosi_felles_egenskaper_linje topo_rein.simple_sosi_felles_egenskaper;
 
-BEGIN
-	
-	-- TODO to be moved is justed for testing now
-	line_topo_info.topology_name := 'topo_rein_sysdata';
-	line_topo_info.layer_schema_name := 'topo_rein';
-	line_topo_info.layer_table_name := 'reindrift_anlegg_linje';
-	line_topo_info.layer_feature_column := 'linje';
-	line_topo_info.snap_tolerance := 0.0000000001;
-	line_topo_info.element_type = 2;
-	-- find line layer id
-	line_layer_id := topo_update.get_topo_layer_id(line_topo_info);
-	
-	DROP TABLE IF EXISTS ttt_new_attributes_values;
+-- array of quoted field identifiers
+-- for attribute fields passed in by user and known (by name)
+-- in the target table
+update_fields text[];
 
-	CREATE TEMP TABLE ttt_new_attributes_values(geom geometry,properties json);
+-- array of quoted field identifiers
+-- for attribute fields passed in by user and known (by name)
+-- in the temp table
+update_fields_t text[];
+
+BEGIN
+
+	-- get meta data
+	topo_info := topo_update.make_input_meta_info(layer_schema, layer_table , layer_column );
 	
-	-- get json data
-	INSERT INTO ttt_new_attributes_values(properties)
+
+	-- get the rows from json_feature into a table
+	DROP TABLE IF EXISTS ttt2_new_attributes_values;
+	CREATE TEMP TABLE ttt2_new_attributes_values(geom geometry,properties json);
+	-- get json data with out geometry properties
+	INSERT INTO ttt2_new_attributes_values(properties)
 	SELECT 
---		topo_rein.get_geom_from_json(feat,4258) as geom,
+-- 		topo_rein.get_geom_from_json(feat,4258) as geom, there is now gemeyry
 		to_json(feat->'properties')::json  as properties
 	FROM (
 	  	SELECT json_feature::json AS feat
 	) AS f;
 
-	--  
-	
-	IF (SELECT count(*) FROM ttt_new_attributes_values) != 1 THEN
+	--  update the variable simple_sosi_felles_egenskaper_linje  with a value from ttt2_new_attributes_values
+	IF (SELECT count(*) FROM ttt2_new_attributes_values) != 1 THEN
 		RAISE EXCEPTION 'Not valid json_feature %', json_feature;
 	ELSE 
-
 		-- TODO find another way to handle this
 		SELECT * INTO simple_sosi_felles_egenskaper_linje 
 		FROM json_populate_record(NULL::topo_rein.simple_sosi_felles_egenskaper,
-		(select properties from ttt_new_attributes_values) );
+		(select properties from ttt2_new_attributes_values) );
 
 	END IF;
 
+	-- Create temporary table ttt2_new_topo_rows_in_org_table to receive the new record
+	command_string := topo_update.create_temp_tbl_as(
+	  'ttt2_new_topo_rows_in_org_table',
+	  format('SELECT * FROM %I.%I LIMIT 0',
+	         topo_info.layer_schema_name,
+	         topo_info.layer_table_name));
+	EXECUTE command_string;
+
+  	-- Insert all matching column names into temp table ttt2_new_topo_rows_in_org_table 
+	INSERT INTO ttt2_new_topo_rows_in_org_table
+		SELECT r.* --, t2.geom 
+		FROM ttt2_new_attributes_values t2,
+         json_populate_record(
+            null::ttt2_new_topo_rows_in_org_table,
+            t2.properties) r;
+	RAISE NOTICE 'Added all attributes to ttt2_new_topo_rows_in_org_table';
+
+	-- Update felles egenskaper with new values
+	command_string := format('UPDATE ttt2_new_topo_rows_in_org_table 
+	SET felles_egenskaper = topo_rein.get_rein_felles_egenskaper_update(r.felles_egenskaper, %L)
+	FROM  %I.%I r',
+	simple_sosi_felles_egenskaper_linje,
+    topo_info.layer_schema_name,
+    topo_info.layer_table_name
+	);
+	RAISE NOTICE 'command_string %', command_string;
+	EXECUTE command_string;
+
+  RAISE NOTICE 'Set felles_egenskaper field';
+
+  -- Extract name of fields with not-null values:
+  SELECT array_agg(quote_ident(key))
+    FROM ttt2_new_topo_rows_in_org_table t, json_each_text(to_json((t)))
+   WHERE key != 'id'
+    INTO update_fields;
+  RAISE NOTICE 'Extract name of not-null fields: %', update_fields;
+  
+  -- Extract name of fields with not-null values and append the table prefix n.:
+  SELECT array_agg('n.'||quote_ident(key))
+    FROM ttt2_new_topo_rows_in_org_table t, json_each_text(to_json((t)))
+   WHERE key != 'id'
+    INTO update_fields_t;
+  RAISE NOTICE 'Extract name of not-null fields: %', update_fields_t;
+  
+  -- update the org table with not null values
+  command_string := format(
+    'UPDATE %I.%I s SET
+	(%s) = (%s) 
+	FROM ttt2_new_topo_rows_in_org_table n WHERE n.id = s.id',
+    topo_info.layer_schema_name,
+    topo_info.layer_table_name,
+    array_to_string(update_fields, ','),
+    array_to_string(update_fields_t, ',')
+    );
+	RAISE NOTICE 'command_string %', command_string;
+	EXECUTE command_string;
 	
-	-- We now know which rows we can reuse clear out old data rom the realation table
-	UPDATE topo_rein.reindrift_anlegg_linje r
-	SET 
-		reindriftsanleggstype = (t2.properties->>'reindriftsanleggstype')::int,
-		reinbeitebruker_id = (t2.properties->>'reinbeitebruker_id')::text,
-		felles_egenskaper = topo_rein.get_rein_felles_egenskaper_update(felles_egenskaper, simple_sosi_felles_egenskaper_linje)
-	FROM ttt_new_attributes_values t2
-	-- WHERE ST_Intersects(r.omrade::geometry,t2.geom);
-	WHERE id = (t2.properties->>'id')::int;
 	
 	GET DIAGNOSTICS num_rows_affected = ROW_COUNT;
 
@@ -89,3 +129,9 @@ $$ LANGUAGE plpgsql;
 
 
 
+--{ kept for backward compatility
+CREATE OR REPLACE FUNCTION  topo_update.apply_attr_on_topo_line(json_feature text) 
+RETURNS TABLE(id integer) AS $$
+  SELECT topo_update.apply_attr_on_topo_line($1, 'topo_rein', 'reindrift_anlegg_linje', 'linje');
+$$ LANGUAGE 'sql';
+--}
