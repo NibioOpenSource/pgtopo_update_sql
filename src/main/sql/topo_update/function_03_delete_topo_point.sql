22cc7950d747ba53b2e17bf2988afe2686b93161
Lars Aksel Opsahl <lop@skogoglandskap.no>
2015-12-23 12:01:37 +0100

    Added generic delete for point layer

--- src/main/sql/topo_update/function_03_delete_topo_point.sql
+++ src/main/sql/topo_update/function_03_delete_topo_point.sql
@@ -7,15 +7,28 @@ RETURNS int AS $$DECLARE
 -- holds the num rows affected when needed
 num_rows_affected int;
 
+-- holds dynamic sql to be able to use the same code for different
+command_string text;
 
 BEGIN
 
-	PERFORM topology.clearTopoGeom(punkt) FROM topo_rein.reindrift_anlegg_punkt r
-	WHERE id = id_in;
+	command_string := format('SELECT topology.clearTopoGeom(%s) FROM  %I.%I r WHERE id = %s',
+	layer_column,
+    layer_schema,
+    layer_table,
+    id_in);
 
+	RAISE NOTICE 'command_string %', command_string;
+	EXECUTE command_string;
 
-	DELETE FROM topo_rein.reindrift_anlegg_punkt r
-	WHERE id = id_in;
+    command_string := format('DELETE FROM %I.%I r
+	WHERE id = %s',
+    layer_schema,
+    layer_table,
+    id_in);
+
+    RAISE NOTICE 'command_string %', command_string;
+	EXECUTE command_string;
 	
 	GET DIAGNOSTICS num_rows_affected = ROW_COUNT;
 
