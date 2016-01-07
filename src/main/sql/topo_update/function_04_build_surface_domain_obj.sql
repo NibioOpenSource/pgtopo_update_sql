aff849d21d67ee7342bbd29ad2ebee68decb4257
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-01-07 12:33:57 +0100

    Made it generic so it can be used on different surface layers

--- src/main/sql/topo_update/function_04_build_surface_domain_obj.sql
+++ src/main/sql/topo_update/function_04_build_surface_domain_obj.sql
@@ -1,7 +1,12 @@
 -- This is a function that is used to build surface based on the border objects that alreday exits
 -- topoelementarray_data is reffrerance to the egdes in this layer
 
--- DROP FUNCTION IF EXISTS topo_update.build_surface_domain_obj(json_feature text) cascade;
+DROP FUNCTION IF EXISTS topo_update.build_surface_domain_obj(
+json_feature text,
+topoelementarray_data topoelementarray, 
+  layer_schema text, 
+  surface_layer_table text, surface_layer_column text,
+  snap_tolerance float8) cascade;
 
 
 CREATE OR REPLACE FUNCTION topo_update.build_surface_domain_obj(
@@ -9,19 +14,13 @@ json_feature text,
 topoelementarray_data topoelementarray, 
   layer_schema text, 
   surface_layer_table text, surface_layer_column text,
-  border_layer_table text, border_layer_column text,
   snap_tolerance float8) 
-RETURNS TABLE(result text) AS $$
+RETURNS TABLE(id integer) AS $$
 DECLARE
 
 json_result text;
 
--- TODO use as parameter put for testing we just have here for now
-border_topo_info topo_update.input_meta_info ;
 surface_topo_info topo_update.input_meta_info ;
-
--- hold striped gei
-edge_with_out_loose_ends geometry = null;
 
 -- holds dynamic sql to be able to use the same code for different
 command_string text;
@@ -29,23 +28,14 @@ command_string text;
 -- used for logging
 num_rows_affected int;
 
--- used for logging
-add_debug_tables int = 0;
-
 -- the number times the inlut line intersects
 num_edge_intersects int;
 
--- the orignal geo that is from the user
-org_geo_in geometry;
-
-geo_in geometry;
-
-line_intersection_result geometry;
-
 -- holds the value for felles egenskaper from input
-felles_egenskaper_linje topo_rein.sosi_felles_egenskaper;
+simple_sosi_felles_egenskaper_flate topo_rein.simple_sosi_felles_egenskaper;
 felles_egenskaper_flate topo_rein.sosi_felles_egenskaper;
-simple_sosi_felles_egenskaper_linje topo_rein.simple_sosi_felles_egenskaper;
+
+surface_topogeometry topogeometry;
 
 -- array of quoted field identifiers
 -- for attribute fields passed in by user and known (by name)
@@ -60,13 +50,10 @@ update_fields_t text[];
 
 BEGIN
 	
-	-- get meta data the border line for the surface
-	border_topo_info := topo_update.make_input_meta_info(layer_schema, border_layer_table , border_layer_column );
-
 	-- get meta data the surface 
 	surface_topo_info := topo_update.make_input_meta_info(layer_schema, surface_layer_table , surface_layer_column );
 	
-	CREATE TEMP TABLE IF NOT EXISTS ttt2_new_attributes_values(geom geometry,properties json, felles_egenskaper topo_rein.sosi_felles_egenskaper);
+	CREATE TEMP TABLE IF NOT EXISTS ttt2_new_attributes_values(properties json, felles_egenskaper topo_rein.sosi_felles_egenskaper);
 	TRUNCATE TABLE ttt2_new_attributes_values;
 	
 	-- parse the json data to get properties and the new geometry
@@ -87,48 +74,120 @@ BEGIN
 		RAISE EXCEPTION 'Not valid json_feature %', json_feature;
 	ELSE 
 		-- get the felles egenskaper
-		SELECT * INTO simple_sosi_felles_egenskaper_linje 
+		SELECT * INTO simple_sosi_felles_egenskaper_flate 
 		FROM json_populate_record(NULL::topo_rein.simple_sosi_felles_egenskaper,
 		(select properties from ttt2_new_attributes_values) );
 
-		-- get felles_egenskaper both for sufcae and line
-		felles_egenskaper_linje := topo_rein.get_rein_felles_egenskaper(simple_sosi_felles_egenskaper_linje);
-		felles_egenskaper_flate := topo_rein.get_rein_felles_egenskaper_flate(simple_sosi_felles_egenskaper_linje);
-
 	END IF;
 
-	-- Create a temp table
+	
+	-- Create a temp table tha will hodl the row will be inserted at the end
 	command_string := topo_update.create_temp_tbl_as(
 	  'ttt2_new_topo_rows_in_org_table',
 	  format('SELECT * FROM %I.%I LIMIT 0',
-	         surface_topo_info.layer_schema_name,
+	  surface_topo_info.layer_schema_name,
 	         surface_topo_info.layer_table_name));
 	EXECUTE command_string;
 
-
+	 -- insert one row to be able create update field
+	insert into ttt2_new_topo_rows_in_org_table(id) values(1); 
 	
+-- Extract name of fields with not-null values:
+  -- Extract name of fields with not-null values and append the table prefix n.:
+  -- Only update json value that exits 
+  SELECT
+  	array_agg(quote_ident(update_column)) AS update_fields,
+  	array_agg('n.'||quote_ident(update_column)) as update_fields_t
+  INTO
+  	update_fields,
+  	update_fields_t
+  FROM (
+   SELECT distinct(key) AS update_column
+   FROM ttt2_new_topo_rows_in_org_table t, json_each_text(to_json((t)))  ,
+   (SELECT json_object_keys(t2.properties) as res FROM ttt2_new_attributes_values t2 ) as key_list
+   WHERE key != 'id' AND 
+   key = key_list.res 
+  ) AS keys;
+
+    
+  RAISE NOTICE 'Extract name of not-null fields: %', update_fields_t;
+  RAISE NOTICE 'Extract name of not-null fields: %', update_fields;
+
+  -- we have got the update filds so we don'n need this row any more
+  truncate ttt2_new_topo_rows_in_org_table;
+  
+	-- get the felles egeskaper flate object
+	felles_egenskaper_flate := topo_rein.get_rein_felles_egenskaper_flate(simple_sosi_felles_egenskaper_flate);
+
+	-- create the topo object 
+	surface_topogeometry := topology.CreateTopoGeom( surface_topo_info.topology_name,surface_topo_info.element_type,surface_topo_info.border_layer_id,topoelementarray_data); 
+
   	-- Insert all matching column names into temp table ttt2_new_topo_rows_in_org_table 
-	INSERT INTO ttt2_new_topo_rows_in_org_table(reinbeitebruker_id,reindrift_sesongomrade_kode,felles_egenskaper,omrade)
+  	command_string := format('INSERT INTO ttt2_new_topo_rows_in_org_table(%s,felles_egenskaper,%s)
 		SELECT 
-		reinbeitebruker_id,
-		reindrift_sesongomrade_kode,
-		felles_egenskaper_flate as felles_egenskaper,
-		topology.CreateTopoGeom( surface_topo_info.topology_name,surface_topo_info.element_type,surface_topo_info.border_layer_id,topoelementarray_data) AS omrade
+		%s,
+		$1 as felles_egenskaper,
+		$2 AS %s
 		FROM ttt2_new_attributes_values t2,
          json_populate_record(
             null::ttt2_new_topo_rows_in_org_table,
-            t2.properties) r;
-	RAISE NOTICE 'Added all attributes to ttt2_new_topo_rows_in_org_table';
+            t2.properties) r',
+    array_to_string(update_fields, ','),
+    surface_topo_info.layer_feature_column,
+    array_to_string(update_fields, ','),
+	surface_topo_info.layer_feature_column);
+
+	RAISE NOTICE 'command_string %' , command_string;
+
+	EXECUTE command_string USING felles_egenskaper_flate, surface_topogeometry;
+	
+	-- Insert the rows in to master table 
+
+	command_string := format('
+	WITH inserted AS ( 
+	INSERT INTO %I.%I(%s,felles_egenskaper,%s)
+	SELECT %s,felles_egenskaper,%s FROM ttt2_new_topo_rows_in_org_table RETURNING * ), 
+	deleted AS ( DELETE FROM ttt2_new_topo_rows_in_org_table ) 
+	INSERT INTO ttt2_new_topo_rows_in_org_table SELECT * FROM inserted ',
+	surface_topo_info.layer_schema_name,
+	surface_topo_info.layer_table_name,
+    array_to_string(update_fields, ','),
+    surface_topo_info.layer_feature_column,
+    array_to_string(update_fields, ','),
+	surface_topo_info.layer_feature_column);
+
+	RAISE NOTICE 'command_string %' , command_string;
+
+	EXECUTE command_string USING felles_egenskaper_flate, surface_topogeometry;
 
 
-	INSERT INTO topo_rein.arstidsbeite_var_flate(reinbeitebruker_id,reindrift_sesongomrade_kode,felles_egenskaper,omrade)
-	SELECT reinbeitebruker_id,reindrift_sesongomrade_kode,felles_egenskaper,omrade FROM ttt2_new_topo_rows_in_org_table;
-
-
-	command_string := 'SELECT json_agg(row_to_json(t.*))::text FROM ttt2_new_topo_rows_in_org_table AS t';
+	command_string := 'SELECT t.id FROM ttt2_new_topo_rows_in_org_table t';
 
     RETURN QUERY EXECUTE command_string;
     
 END;
 $$ LANGUAGE plpgsql;
+
+
+--SELECT topo_update.create_line_edge_domain_obj(json,'topo_rein', 'arstidsbeite_var_grense', 'grense', 1e-10)
+--FROM org_rein_sosi_dump.arstidsbeite_json_grense_v g ,org_rein_sosi_dump.rein_sosi_dump_arstidsbeite_var_flate f
+--WHERE f.objectid = 5358 AND g.geo && f.geo and ST_intersects(f.geo,g.geo)
+
+
+--SELECT topo_update.build_surface_domain_obj(r2.json,r1.topoelementarray,'topo_rein', 'arstidsbeite_var_flate', 'omrade', 1e-10) FROM
+--(
+--	select distinct ST_GetFaceGeometry('topo_rein_sysdata',l.face_id) as geo,
+--		topology.TopoElementArray_Agg(ARRAY[l.face_id,3]) as topoelementarray, 
+--		ST_union(l.mbr) as union_face
+--	from 
+--	topo_rein_sysdata.face as l
+--	 WHERE l.mbr is not null
+--	 group by l.face_id
+--	 limit 1
+--) AS r1,
+--org_rein_sosi_dump.arstidsbeite_var_json_flate_v r2
+--WHERE r2.geo && r1.geo
+--AND ST_Covers(r2.geo,ST_PointOnSurface(r1.geo));
+
+-- SELECT * from topo_rein.arstidsbeite_var_flate;
 
