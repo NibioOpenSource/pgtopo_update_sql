0396aa870a3741710b07e8a70b47d2df795b43a2
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-01-08 23:54:11 +0100

    Use new inport script

---
+++ src/test/sql/help_scripts/copy_all_data_from_tmp.sql
@@ -0,0 +1,6 @@
+
+-- topo_rein.arstidsbeite_var_flate
+SELECT  org_rein_sosi_dump.simplefeature_2_topo_surface(
+'topo_rein', 'arstidsbeite_var_flate', 'omrade', 'org_rein_sosi_dump.arstidsbeite_var_json_flate_v', 
+'arstidsbeite_var_grense','grense', 'org_rein_sosi_dump.arstidsbeite_var_json_grense_v', 1e-10,
+ null);
