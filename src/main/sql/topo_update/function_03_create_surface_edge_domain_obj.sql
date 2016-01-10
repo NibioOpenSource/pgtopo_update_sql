2ed0178bea9f395649f01d7cff7fa2f6862c862c
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-01-10 08:05:05 +0100

    Fixed to work with topo layers

--- src/main/sql/topo_update/function_03_create_surface_edge_domain_obj.sql
+++ src/main/sql/topo_update/function_03_create_surface_edge_domain_obj.sql
@@ -138,7 +138,10 @@ BEGIN
 		-- TODO handle return of gemoerty collection
 		-- thic code fails need to make a test on this 
 		-- num_edge_intersects :=  (SELECT ST_NumGeometries(ST_Intersection(geo_in,e.geom)) FROM topo_rein_sysdata.edge_data e WHERE ST_Intersects(geo_in,e.geom))::int;
-		line_intersection_result := (select ST_Union(ST_Intersection(geo_in,e.geom)) FROM topo_rein_sysdata.edge_data e WHERE ST_Intersects(geo_in,e.geom))::geometry;
+		command_string := format('(select ST_Union(ST_Intersection(geo_in,e.geom)) FROM topo_rein_sysdata.edge_data e WHERE ST_Intersects(geo_in,e.geom))::geometry',
+  		border_topo_info.layer_schema_name);
+    
+  		EXECUTE command_string INTO line_intersection_result;
 
 		RAISE NOTICE 'Line intersection result is %', ST_AsText(line_intersection_result);
 
