98548cf151142886debce4560e37f056ee751aba
Lars Aksel Opsahl <lop@skogoglandskap.no>
2016-05-31 10:14:47 +0200

    Added more comments and some code cleanup

--- src/main/sql/topo_update/function_03_create_surface_edge_domain_obj.sql
+++ src/main/sql/topo_update/function_03_create_surface_edge_domain_obj.sql
@@ -67,15 +67,17 @@ BEGIN
 	-- get meta data the surface 
 	surface_topo_info := topo_update.make_input_meta_info(layer_schema, surface_layer_table , surface_layer_column );
 	
-	-- parse the input values
+	-- parse the input values and find input geo and properties. 
+	-- Equal properties found both in client_json_feature and server_json_feature, then the values in server_json_feature will be used.
 	json_input_structure := topo_update.handle_input_json_props(client_json_feature::json,server_json_feature::json,4258);
 
-	-- save a copy of the input geometry
+	-- save a copy of the input geometry before modfied, used for logging later.
 	org_geo_in := json_input_structure.input_geo;
 	geo_in := json_input_structure.input_geo;
+	
 	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj The input as it used before check/fixed %',  ST_AsText(geo_in);
 
-		-- Only used for debug
+	-- Only used for debug
 	IF add_debug_tables = 1 THEN
 		-- get new objects created from topo_update.create_edge_surfaces
 		DROP TABLE IF EXISTS topo_rein.create_surface_edge_domain_obj_t0; 
@@ -83,7 +85,7 @@ BEGIN
 		INSERT INTO topo_rein.create_surface_edge_domain_obj_t0(geo_in,IsSimple,IsClosed) VALUES(geo_in,St_IsSimple(geo_in),St_IsSimple(geo_in));
 	END IF;
 	
-	-- modify the input if needed
+	-- modify the input if it's not simple.
 	IF NOT ST_IsSimple(geo_in) THEN
 		-- This is probably a crossing line so we try to build a surface
 		BEGIN
@@ -122,30 +124,24 @@ BEGIN
 		END IF;
 	END IF;
 
+	-- check that is not null
+	IF geo_in IS NULL THEN
+		RAISE EXCEPTION 'The geo generated from geo_in is null %', org_geo_in;
+	END IF;
+
+	-- The geo_in is now modified and we know that it is a valid geo we can use it to create the new border. 
+
 	IF add_debug_tables = 1 THEN
 		INSERT INTO topo_rein.create_surface_edge_domain_obj_t0(geo_in,IsSimple,IsClosed) VALUES(geo_in,St_IsSimple(geo_in),St_IsSimple(geo_in));
 	END IF;
 
 	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj The input as it used after check/fixed %',  ST_AsText(geo_in);
 	
-	IF geo_in IS NULL THEN
-		RAISE EXCEPTION 'The geo generated from geo_in is null %', org_geo_in;
-	END IF;
-
-	-- The geo_in is now modified and we can use it
-	
-	-- create the new topo object for the egde layer, this edges will be used by the new surface objects later
+	-- Create the new topo object for the egde layer, this edges will be used by the new surface objects later
 	new_border_data := topo_update.create_surface_edge(geo_in,border_topo_info);
 	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj The new topo object created for based on the input geo % in table %.%',  new_border_data, border_topo_info.layer_schema_name,border_topo_info.layer_table_name;
 	
-	-- perpare table for rows to be returned to the caller 
-	DROP TABLE IF EXISTS create_surface_edge_domain_obj_r1_r; 
-	CREATE TEMP TABLE create_surface_edge_domain_obj_r1_r(id int, id_type text) ;
-	
-	
-	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Step::::::::::::::::: 2';
-
-	-- Create temporary table to receive the new record
+	-- Create temporary table to hold the new data for the border objects. We here use the same table structure as the restult table.
 	command_string := topo_update.create_temp_tbl_as(
 	  'ttt2_new_topo_rows_in_org_table',
 	  format('SELECT * FROM %I.%I LIMIT 0',
@@ -153,65 +149,79 @@ BEGIN
 	         border_topo_info.layer_table_name));
 	EXECUTE command_string;
 
-  -- Insert all matching column names into temp table
-    INSERT INTO ttt2_new_topo_rows_in_org_table
+  	-- Insert a single row into border temp table using the columns from json input that match column names in the temp table craeted
+	INSERT INTO ttt2_new_topo_rows_in_org_table
 	SELECT * FROM json_populate_record(null::ttt2_new_topo_rows_in_org_table,json_input_structure.json_properties);
+	
+	-- TODO add a test to be sure that only a single row is inserted,
 
-  RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Added all attributes to ttt2_new_topo_rows_in_org_table';
+	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Added all attributes to ttt2_new_topo_rows_in_org_table';
 
-  
-  
-
-
-  -- Convert geometry to TopoGeometry, write it in the temp table
-  command_string := format('UPDATE ttt2_new_topo_rows_in_org_table
+	-- Update the single rows in border line temp table with TopoGeometry and felles egenskaper
+	command_string := format('UPDATE ttt2_new_topo_rows_in_org_table
     SET %I = %L,
 	 felles_egenskaper = %L',
-  border_topo_info.layer_feature_column, new_border_data,json_input_structure.sosi_felles_egenskaper);
-    
-  EXECUTE command_string;
+  	border_topo_info.layer_feature_column, new_border_data,json_input_structure.sosi_felles_egenskaper);
+  	EXECUTE command_string;
 
-  RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Set felles_egenskaper field';
+  	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Set felles_egenskaper field';
 
-  -- Extract name of fields with not-null values:
-  SELECT array_agg(quote_ident(key))
-    FROM ttt2_new_topo_rows_in_org_table t, json_each_text(to_json((t)))
-   WHERE value IS NOT NULL
-    INTO not_null_fields;
+	-- Find name of columns with not-null values from the temp table
+	-- We need this list of column names to crete a SQL to update the orignal row with new values.
+	SELECT array_agg(quote_ident(key))
+	  FROM ttt2_new_topo_rows_in_org_table t, json_each_text(to_json((t)))
+	WHERE value IS NOT NULL
+	  INTO not_null_fields;
 
-  RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Extract name of not-null fields: %', not_null_fields;
+	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Extract name of not-null fields: %', not_null_fields;
 
-  -- Copy full record from temp table to actual table and
-  -- update temp table with actual table values
-  command_string := format(
-    'WITH inserted AS ( INSERT INTO %I.%I (%s) SELECT %s FROM
-ttt2_new_topo_rows_in_org_table RETURNING * ), deleted AS ( DELETE
-FROM ttt2_new_topo_rows_in_org_table ) INSERT INTO
-ttt2_new_topo_rows_in_org_table SELECT * FROM inserted ',
-    border_topo_info.layer_schema_name,
-    border_topo_info.layer_table_name,
-    array_to_string(not_null_fields, ','),
-    array_to_string(not_null_fields, ',')
-    );
+	-- Copy data from from temp table in to actual table and
+	-- update temp table with actual data stored in actual table. 
+	-- We will then get values for id's and default values back in to the temp table.
+	command_string := format(
+	    'WITH inserted AS ( INSERT INTO %I.%I (%s) SELECT %s FROM
+		ttt2_new_topo_rows_in_org_table RETURNING * ), deleted AS ( DELETE
+		FROM ttt2_new_topo_rows_in_org_table ) INSERT INTO
+		ttt2_new_topo_rows_in_org_table SELECT * FROM inserted ',
+	    border_topo_info.layer_schema_name,
+	    border_topo_info.layer_table_name,
+	    array_to_string(not_null_fields, ','),
+	    array_to_string(not_null_fields, ',')
+	);
 	EXECUTE command_string;
 
 	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Step::::::::::::::::: 3';
 	
+	-- Create table for the rows to be returned to the caller.
+	-- The result contains list of line and surface id so the client knows alle row created. 
+	DROP TABLE IF EXISTS create_surface_edge_domain_obj_r1_r; 
+	CREATE TEMP TABLE create_surface_edge_domain_obj_r1_r(id int, id_type text) ;
 	
+	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Step::::::::::::::::: 2';
+
+	-- Insert new line objects created
 	INSERT INTO create_surface_edge_domain_obj_r1_r(id,id_type)
 	SELECT id, 'L' as id_type FROM ttt2_new_topo_rows_in_org_table;
 	
-	-- create the new topo object for the surfaces
+	-- ##############################################################
+	-- We are now done with border line objects and we can start to work on the surface objects
+	-- The new faces are already created so we new find them and relate our domain objects
+	-- ##############################################################
+	
+	-- Create a new temp table to hold topo surface objects that has a relation to the edge added by the user .
 	DROP TABLE IF EXISTS new_surface_data_for_edge; 
 	-- find out if any old topo objects overlaps with this new objects using the relation table
 	-- by using the surface objects owned by the both the new objects and the exting one
 	CREATE TEMP TABLE new_surface_data_for_edge AS 
 	(SELECT topo::topogeometry AS surface_topo, json_input_structure.sosi_felles_egenskaper_flate AS felles_egenskaper_flate 
 	FROM topo_update.create_edge_surfaces(surface_topo_info,border_topo_info,new_border_data,geo_in,json_input_structure.sosi_felles_egenskaper_flate));
+	-- We now have a list with all surfaces that intersect the line that is drwan by the user. 
+	-- In this list there may areas that overlaps so we need to clean up some values
+	
 	GET DIAGNOSTICS num_rows_affected = ROW_COUNT;
 	RAISE NOTICE 'topo_update.create_surface_edge_domain_obj Number of topo surfaces added to table new_surface_data_for_edge   %',  num_rows_affected;
 	
-	-- clean up old surface and return a list of the objects
+	-- Clean up old surface and return a list of the objects that should be returned to the user for further processing
 	DROP TABLE IF EXISTS res_from_update_domain_surface_layer; 
 	CREATE TEMP TABLE res_from_update_domain_surface_layer AS 
 	(SELECT topo::topogeometry AS surface_topo FROM topo_update.update_domain_surface_layer(surface_topo_info,border_topo_info,'new_surface_data_for_edge'));
