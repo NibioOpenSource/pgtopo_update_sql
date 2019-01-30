5e64a8982d5735a474dbe9793faf40651826386c
Lars Aksel Opsahl <lop@skogoglandskap.no>
2019-01-30 18:00:38 +0100

    Update interface for function

--- src/main/sql/topo_update/function_03_layer_accept_update.sql
+++ src/main/sql/topo_update/function_03_layer_accept_update.sql
@@ -1,7 +1,7 @@
 
 
 -- set status to 10 for givene object
-CREATE OR REPLACE FUNCTION topo_update.layer_accept_update(_data_update_log_id_after int, _row_id int, _saksbehandler varchar ) 
+CREATE OR REPLACE FUNCTION topo_update.layer_accept_update(_data_update_log_id_after int, _saksbehandler varchar ) 
 RETURNS int AS $$DECLARE
 
 num_rows_affected int;
@@ -13,29 +13,29 @@ this_schema_name text;
 
 this_table_name text;
 
+this_data_row_id int;
+
 BEGIN
 
+
 	-- get schema name and table nam,e value
-	select s.schema_name, s.table_name into this_schema_name, this_table_name 
+	select s.schema_name, s.table_name, s.row_id 
+	into this_schema_name, this_table_name, this_data_row_id
 	from topo_rein.data_update_log s
-	where row_id = _row_id and change_confirmed_by_admin = false;
+	where s.id = _data_update_log_id_after and s.change_confirmed_by_admin = false;
 
 	command_string := format('update %I.%I set status = 1, saksbehandler = %L where id = %L',
-	this_schema_name, this_table_name, _saksbehandler, _row_id);
-
+	this_schema_name, this_table_name, _saksbehandler, this_data_row_id);
 	RAISE NOTICE 'command_string %' , command_string;
-
 	EXECUTE command_string;
 
 	update topo_rein.data_update_log s
 	set change_confirmed_by_admin = true
-	where row_id = _row_id and change_confirmed_by_admin = false;
-
+	where row_id = this_data_row_id and change_confirmed_by_admin = false;
 	GET DIAGNOSTICS num_rows_affected = ROW_COUNT;
 	RAISE NOTICE 'Number of meta rows affected  %',  num_rows_affected;
 	
 
-	
 	RETURN num_rows_affected;
 
 END;
