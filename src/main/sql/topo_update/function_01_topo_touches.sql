152fadc12676210aeaac1770946340233e65071c
Lars Aksel Opsahl <lop@skogoglandskap.no>
2020-11-03 06:59:51 +0100

    Format and nameing

--- src/main/sql/topo_update/function_01_topo_touches.sql
+++ src/main/sql/topo_update/function_01_topo_touches.sql
@@ -26,35 +26,35 @@ CREATE TEMP TABLE IF NOT EXISTS idlist_temp(id_t int[]);
 --CREATE TEMP TABLE  idlist_temp(id_t int[]);
 
 command_string := format('INSERT INTO idlist_temp(id_t) 
-SELECT id_list as id_t 
-FROM ( SELECT array_agg( object_id) id_list, count(*) as antall
+SELECT object_id_list as id_t 
+FROM ( SELECT array_agg( object_id) object_id_list, count(*) as antall
   FROM (
 	WITH faces AS (
 	    SELECT (GetTopoGeomElements(%1$s))[1] face_id, id AS object_id FROM (
 	      SELECT %1$s, id from  %2$s 
 	    ) foo
-	  ),
-	ary AS ( 
+	),
+	faces_group_by_face_id_object_id AS ( 
 	    SELECT array_agg(face_id) ids, face_id, object_id FROM faces
 	    GROUP BY face_id, object_id
-	  )
+	)
 	SELECT object_id, face_id, e.edge_id 
-	FROM %3$I.edge e, ary f
+	FROM %3$I.edge e, faces_group_by_face_id_object_id f
 	WHERE ( left_face = any (f.ids) and not right_face = any (f.ids) )
 	OR ( right_face = any (f.ids) and not left_face = any (f.ids) )
   ) AS t
   GROUP BY edge_id
 ) AS r
 WHERE antall > 1
-AND id_list[1] != id_list[2]
-AND (id_list[1] = %4$L OR id_list[2] =%4$L)
+AND object_id_list[1] != object_id_list[2]
+AND (object_id_list[1] = %4$L OR object_id_list[2] =%4$L)
 ORDER BY id_t',
 surface_topo_info.layer_feature_column,
 _new_topo_objects,
 surface_topo_info.topology_name,
 id_to_check);
 
-RAISE NOTICE 'command_string %',  command_string;
+--RAISE NOTICE 'command_string touches %',  command_string;
 
 EXECUTE command_string;
 
@@ -66,6 +66,8 @@ INSERT INTO idlist(id) SELECT id_t[1] AS id FROM idlist_temp WHERE id_to_check !
 INSERT INTO idlist(id) SELECT id_t[2] AS id FROM idlist_temp WHERE id_to_check != id_t[2];
 
 SELECT id FROM idlist limit 1 into res;
+
+RAISE NOTICE 'command_string touches result is % with (%) ', res, command_string;
 
 RETURN res;
 
