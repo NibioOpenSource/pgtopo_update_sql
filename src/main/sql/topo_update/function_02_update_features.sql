27a5246d2ef4ae665b16c08100ccba12c6d27eda
Sandro Santilli <strk@kbt.io>
2021-01-19 11:32:18 +0100

    Put json_props_to_pg_cols function in own file and testcase
    Closes #25

--- src/main/sql/topo_update/function_02_update_features.sql
+++ src/main/sql/topo_update/function_02_update_features.sql
@@ -1,35 +1,3 @@
-CREATE OR REPLACE FUNCTION topo_update.json_props_to_pg_cols(
-  props JSON,
-  col_map JSON,
-  OUT colnames TEXT[],
-	OUT colvals TEXT[]
-)
-AS $BODY$
-DECLARE
-	geom GEOMETRY;
-	rec RECORD;
-	val TEXT;
-  json_prop_array TEXT[];
-BEGIN
-
-  colnames := array_agg(k) FROM (
-      SELECT format('%I', json_object_keys( col_map )) k
-    ) f;
-
-  -- TODO: improve performance of this
-  FOR rec IN SELECT json_object_keys( col_map ) k
-  LOOP
-    json_prop_array := array_agg(x) from json_array_elements_text( col_map -> rec.k ) x;
-    --RAISE NOTICE 'prop_array: %', json_prop_array;
-    val := props #>> json_prop_array;
-    --RAISE NOTICE 'val: %', val;
-    colvals := array_append(colvals, format('%L', val));
-  END LOOP;
-
-	--RETURN ( colnames, colvals );
-END;
-$BODY$ LANGUAGE 'plpgsql';
-
 CREATE OR REPLACE FUNCTION topo_update.insert_feature(
   feature JSON,
   srid INT,
