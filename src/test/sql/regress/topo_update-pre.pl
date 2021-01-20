31ccb1ec67e34eab6b9cc2b342172c1d71e2edc1
Sandro Santilli <strk@kbt.io>
2021-01-20 15:34:49 +0100

    Add some error handling, and reduce noise

--- src/test/sql/regress/topo_update-pre.pl
+++ src/test/sql/regress/topo_update-pre.pl
@@ -6,6 +6,9 @@ use warnings;
 my $pre_filename = "topo_update-pre.sql";
 my $cmd = "perl topo_common-pre.pl $pre_filename";
 
-print "\nStart $cmd \n";
-system "$cmd" ;
-print "\nDone $cmd \n";
+#print "\nStart $cmd \n";
+my $exit_code = system "$cmd";
+$exit_code == 0;
+
+#print "\nDone $cmd \n";
+
