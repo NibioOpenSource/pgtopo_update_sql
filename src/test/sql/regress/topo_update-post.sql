58e220c54d37e6484eb0d14b3f3e66338513d8dd
Sandro Santilli <strk@kbt.io>
2021-01-26 17:45:01 +0100

    Put topo_rein and topo_ar5 uninstall scripts near creation scripts

--- src/test/sql/regress/topo_update-post.sql
+++ src/test/sql/regress/topo_update-post.sql
@@ -1,35 +1,7 @@
---DROP OWNED BY topo_rein cascade;
---DROP OWNED BY topo_update cascade;
 
-drop schema topo_rein cascade;
-select topology.droptopology('topo_rein_sysdata');
-select topology.droptopology('topo_rein_sysdata_rvr');
-select topology.droptopology('topo_rein_sysdata_rso');
-select topology.droptopology('topo_rein_sysdata_rhs');
-select topology.droptopology('topo_rein_sysdata_rhv');
-select topology.droptopology('topo_rein_sysdata_rvi');
+\i :regdir/../../data/topo_rein_drop.sql
+\i :regdir/../../data/topo_ar5_drop.sql
 
-select topology.droptopology('topo_rein_sysdata_rbh');
-select topology.droptopology('topo_rein_sysdata_rop');
-select topology.droptopology('topo_rein_sysdata_ran');
-select topology.droptopology('topo_rein_sysdata_rtr');
-
-select topology.droptopology('topo_rein_sysdata_rav');
-select topology.droptopology('topo_rein_sysdata_reo');
-select topology.droptopology('topo_rein_sysdata_rks');
-select topology.droptopology('topo_rein_sysdata_rko');
-select topology.droptopology('topo_rein_sysdata_rdg');
-select topology.droptopology('topo_rein_sysdata_reb');
-select topology.droptopology('topo_rein_sysdata_rro');
-select topology.droptopology('topo_rein_sysdata_rsi');
-
-select topology.droptopology('topo_rein_sysdata_rdr');
-
--- Drop AR5 web client data
-drop schema topo_ar5 cascade;
-select topology.droptopology('topo_ar5_sysdata_webclient');
-
-
--- Drop function schema 
+-- Drop function schema
 drop schema topo_update cascade;
 
