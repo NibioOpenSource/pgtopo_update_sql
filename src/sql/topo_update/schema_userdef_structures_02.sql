54317d6eb7a6324159afca0d6ca95f7a8839e24d
Sandro Santilli <strk@kbt.io>
2021-01-27 10:31:34 +0100

    Style only change, conform to editorconfig

--- src/sql/topo_update/schema_userdef_structures_02.sql
+++ src/sql/topo_update/schema_userdef_structures_02.sql
@@ -2,31 +2,31 @@
 
 ---------------------------------------------------------------------------------
 
--- A composite type to hold infor about the currrent layers that will be updated 
+-- A composite type to hold infor about the currrent layers that will be updated
 -- this will be used to pick up meta info from the topolgy layer doing a update
-CREATE TYPE topo_update.input_meta_info 
+CREATE TYPE topo_update.input_meta_info
 AS (
 	-- refferes to topology.topology
 	topology_name varchar,
-	
+
 	-- reffers to topology.layer
 	layer_schema_name varchar,
 	layer_table_name varchar,
 	layer_feature_column varchar,
 
-	-- For a edge this is 2 and for a surface this is 3 
+	-- For a edge this is 2 and for a surface this is 3
 	element_type int,
 
-	-- this is the snapp to tolerance used for snap to when adding new vector data 
+	-- this is the snapp to tolerance used for snap to when adding new vector data
 	-- a typical value used for degrees is 0.0000000001
 	snap_tolerance float8,
-	
+
 	-- this is computed by using function topo_update.get_topo_layer_id
 	border_layer_id int,
-	
+
 	-- refferes to topology.topology
 	srid  int
-	
+
 
 );
 
@@ -34,9 +34,9 @@ AS (
 
 ---------------------------------------------------------------------------------
 
--- A composite type to hold infor about the currrent layers that will be updated 
+-- A composite type to hold information about the currrent layers that will be updated
 -- this will be used to pick up meta info from the topolgy layer doing a update
-CREATE TYPE topo_update.json_input_structure 
+CREATE TYPE topo_update.json_input_structure
 AS (
 
 -- the input geo picked from the client properties
